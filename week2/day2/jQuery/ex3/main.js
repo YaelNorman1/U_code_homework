474b131a03c6cfe37762f8edec022d70532f6bba
YaelNorman1 <YaelNorman2808@gmail.com>
2022-08-23 09:39:55 +0300

    commiting correct file order

--- week2/day2/jQuery/ex3/main.js
+++ week2/day2/jQuery/ex3/main.js
@@ -1,11 +1,21 @@
-$("body").append("<span class='box' id='box1'></span> <span class='box' id='box2'></span>")
+// $("body").append("<span class='box' id='box1'></span> <span class='box' id='box2'></span>")
 
-$("body").on("mouseover","#box1", function(){
-    $("#box2").css("background-color", "red");
-    $("#box1").css("background-color", "#8e44ad");
-})
+// $("body").on("mouseover","#box1", function(){
+//     $("#box2").css("background-color", "red");
+//     $("#box1").css("background-color", "#8e44ad");
+// })
 
-$("body").on("mouseover","#box2", function(){
-    $("#box1").css("background-color", "red");
-    $("#box2").css("background-color", "#8e44ad");
+// $("body").on("mouseover","#box2", function(){
+//     $("#box1").css("background-color", "red");
+//     $("#box2").css("background-color", "#8e44ad");
+// })
+
+$("body").append("<div class='box'></div>")
+$("body").append("<div class='box'></div>")
+    
+let $boxes = $(".box")
+    
+$(".box").hover(function () {
+  $boxes.css("background-color", "rgb(231, 76, 60)")
+  $(this).css("background-color", "rgb(142, 68, 173)")
 })
