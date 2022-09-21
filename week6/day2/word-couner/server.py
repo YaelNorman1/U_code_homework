d2ab4b647d48a07ebd7cd6e8cceac9fb18237193
YaelNorman1 <YaelNorman2808@gmail.com>
2022-09-21 14:09:05 +0300

    done

--- week6/day2/word-couner/server.py
+++ week6/day2/word-couner/server.py
@@ -2,19 +2,6 @@ from urllib import response
 from fastapi import FastAPI
 from fastapi import Request
 from fastapi import Response, status
-
-app = FastAPI()
-
-tasks = {"foo": "Listen to the Bar Fighters"}
-
-
-@app.put("/get-or-create-task/{task_id}", status_code=200)
-def get_or_create_task(task_id: str, response: Response):
-    if task_id not in tasks:
-        tasks[task_id] = "This didn't exist before"
-        response.status_code = status.HTTP_201_CREATED
-    return tasks[task_id]
-
 import uvicorn
 
 app = FastAPI()
