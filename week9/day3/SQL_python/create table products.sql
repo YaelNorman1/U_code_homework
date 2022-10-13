3fae6121be66f9ecd8951fa3737fa6b5e96abfc1
YaelNorman1 <YaelNorman2808@gmail.com>
2022-10-13 14:27:50 +0300

    SQL- python

---
+++ week9/day3/SQL_python/create table products.sql
@@ -0,0 +1,15 @@
+USE grocery_store;
+
+
+-- CREATE TABLE products(
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+--     name VARCHAR(20), 
+--     price INT, 
+--     category_name VARCHAR(20),
+--     FOREIGN KEY (category_name) REFERENCES categories(name)
+--     );
+
+-- INSERT INTO products(id, name, price, category_name) 
+-- VALUES (null, "cheese", 5, "milk")
+
+SELECT * FROM products
